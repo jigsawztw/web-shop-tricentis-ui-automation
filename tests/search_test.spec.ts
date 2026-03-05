0576bf11980f56cc1471c5704a26a2b38c0b98b8
Ivan Osipov <bamby4a364@gmail.com>
2026-03-06 00:52:41 +0300

    удалил файл тестовой стратегии, смоуки и регресс  из корня проекта

--- tests/search_test.spec.ts
+++ tests/search_test.spec.ts
@@ -2,7 +2,9 @@ import { test, expect } from '@playwright/test';
 
 test('TC-REGRESSION-03: Поиск товара ', async ({ page }) => {
     const products = [
-        'Custom T-Shir'
+        'Custom T-Shirt',
+        '14.1-inch Laptop',
+        'Camcorder'
     ];
 
     const productName = products[Math.floor(Math.random() * products.length)];
@@ -10,7 +12,7 @@ test('TC-REGRESSION-03: Поиск товара ', async ({ page }) => {
     await page.goto('https://demowebshop.tricentis.com/');
     await page.locator('#small-searchterms').fill(productName);
     await page.getByRole('button', { name: 'Search' }).click();
-
+    
     await expect(
         page.locator(`//a[text()="${productName}"]`)
     ).toBeVisible();
