8a017c4a7a30944301688958cad140cdbe37c38f
Ivan Osipov <bamby4a364@gmail.com>
2026-03-06 23:37:35 +0300

    поправил немного конфиг

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,61 +1,42 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: './tests',
-  /* Run tests in files in parallel */
-  fullyParallel: false,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  fullyParallel: true,   // параллельный запуск
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  //reporter: 'html',
-  reporter: [['list', { printSteps: true }]], // вывод шагов
-  reporter: [['html', { outputFolder: `playwright-report-${Date.now()}` }]],
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  retries: process.env.CI ? 1 : 0,
+  // workers: 1,             // один поток для последовательного выполнения
+  reporter: [
+    ['list', { printSteps: true }], //видеть шаги в консоли
+    ['html', { outputFolder: `playwright-report-${Date.now()}` }] //генерация папки с отчетом в отдельную папку
+  ],
   use: {
-    headless: false,           // можно false, если хочешь видеть браузер при запуске
     screenshot: 'only-on-failure',
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    //trace: 'on-first-retry',
+    launchOptions: {
+      /*  slowMo: 500, */        // замедляем действия для наглядного дебага
+    },
   },
 
-  /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'],
-        /* launchOptions: {
-        slowMo: 1700
+      use: {
+        ...devices['Desktop Chrome'],
+        launchOptions: {
+          headless: true,
+          /*  slowMo: 500, */
+        },
+      },
+    },
+    /*  {
+        name: 'firefox',
+        use: { ...devices['Desktop Firefox'] },
+      },
+  
+      {
+        name: 'webkit',
+        use: { ...devices['Desktop Safari'] },
       }, */
-       },
-    },
-
-   /*  {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    }, */
 
     /* Test against mobile viewports. */
     // {
@@ -75,13 +56,6 @@ export default defineConfig({
     // {
     //   name: 'Google Chrome',
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    // },  
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
