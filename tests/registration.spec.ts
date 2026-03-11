dec46123494293b4c58fb7c18200ab06da2c73c1
Ivan Osipov <bamby4a364@gmail.com>
2026-03-12 01:36:26 +0300

    поправил шаги негативных сценариев тестов регистрации

--- tests/registration.spec.ts
+++ tests/registration.spec.ts
@@ -5,17 +5,22 @@ import { RegisterPage, MainPage } from "../src/pages/index";
 
 test.describe("Регистрация пользователя", () => {
   let user: User;
+  let mainPage: MainPage;
+  let registerPage: RegisterPage;
+
   test.beforeEach(async ({ page }) => {
     user = userFactory.createByDefault();
+
+    mainPage = new MainPage(page);
+    registerPage = new RegisterPage(page);
   });
 
   test("TC-REGRESSION-04: Успешная регистрация пользователя", async ({
     page,
   }) => {
-    const mainPage = new MainPage(page);
     await mainPage.open();
     await mainPage.goToRegisterPage();
-    const registerPage = new RegisterPage(page);
+
     await registerPage.openAndCheckForm();
     await registerPage.fillRegisterFields({
       firstName: user.firstName,
@@ -41,13 +46,17 @@ test.describe("Регистрация пользователя", () => {
   test("TC-REGRESSION-05: Невалидный email", async ({ page }) => {
     user.email = "@s";
 
-    await page.locator("#gender-female").click();
-    await page.fill("#FirstName", user.firstName);
-    await page.fill("#LastName", user.lastName);
-    await page.fill("#Email", user.email);
-    await page.fill("#Password", user.password);
-    await page.fill("#ConfirmPassword", user.password);
-    await page.locator("#register-button").click();
+    await mainPage.open();
+    await mainPage.goToRegisterPage();
+
+    await registerPage.openAndCheckForm();
+    await registerPage.fillRegisterFields({
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      password: user.password,
+    });
+    await registerPage.register();
 
     await expect(page.locator(".field-validation-error span")).toHaveText(
       /wrong email/i,
@@ -57,13 +66,17 @@ test.describe("Регистрация пользователя", () => {
   test("TC-REGRESSION-06: Пустой Email", async ({ page }) => {
     user.email = "";
 
-    await page.locator("#gender-female").click();
-    await page.fill("#FirstName", user.firstName);
-    await page.fill("#LastName", user.lastName);
-    await page.fill("#Email", user.email);
-    await page.fill("#Password", user.password);
-    await page.fill("#ConfirmPassword", user.password);
-    await page.locator("#register-button").click();
+    await mainPage.open();
+    await mainPage.goToRegisterPage();
+
+    await registerPage.openAndCheckForm();
+    await registerPage.fillRegisterFields({
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      password: user.password,
+    });
+    await registerPage.register();
 
     await expect(page.locator(".field-validation-error span")).toHaveText(
       /email is required./i,
