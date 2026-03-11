600849bb177f26cb2ff217716c53033207b02ccd
Ivan Osipov <bamby4a364@gmail.com>
2026-03-11 20:19:54 +0300

    поправил формат

--- factories/userFactory.ts
+++ factories/userFactory.ts
@@ -13,39 +13,39 @@ export const userFactory = {
       password: faker.internet.password({ length: 6 }),
     };
   },
-    //to-do - возможно придется вернуть позже, либо удалить за ненадобностью
-  /* createWithEmail(email: string): User {
-    const user = this.createByDefault()
-    return {
-      ...user,
-      email
-    }
-  },
-
-  createWithInvalidEmail(): User {
-    const invalidEmails = [
-      'invalid-email',
-      'test@',
-      '@gmail.com',
-      'testgmail.com',
-      'test@.com'
-    ];
-
-    const randomInvalid =
-      invalidEmails[Math.floor(Math.random() * invalidEmails.length)];
-
-    const user = this.createByDefault();
-
-    return {
-      ...user,
-      email: randomInvalid
-    };
-  },  */
-
-  createWithOverride(overrides: Partial<User>): User {
-    return {
-      ...this.createByDefault(),
-      ...overrides,
-    };
-  },
+  //to-do - возможно придется вернуть позже, либо удалить за ненадобностью
+  /*  createWithEmail(email: string): User {
+     const user = this.createByDefault()
+     return {
+       ...user,
+       email
+     }
+   },
+ 
+   createWithInvalidEmail(): User {
+     const invalidEmails = [
+       'invalid-email',
+       'test@',
+       '@gmail.com',
+       'testgmail.com',
+       'test@.com'
+     ];
+ 
+     const randomInvalid =
+       invalidEmails[Math.floor(Math.random() * invalidEmails.length)];
+ 
+     const user = this.createByDefault();
+ 
+     return {
+       ...user,
+       email: randomInvalid
+     };
+   }, 
+ 
+   createWithOverride(overrides: Partial<User>): User {
+     return {
+       ...this.createByDefault(),
+       ...overrides,
+     };
+   }, */
 };
