805f934824e1bcb5ed64edfbda548632ffaedded
Ivan Osipov <bamby4a364@gmail.com>
2026-03-12 00:52:23 +0300

    Добавил класс главной страницы магазина

--- src/pages/main.page.ts
+++ src/pages/main.page.ts
@@ -1,8 +1,19 @@
-import { BasePage } from './base.page'
-import { Page } from '@playwright/test'
+import { BasePage } from "./base.page";
+import { Page, Locator } from "@playwright/test";
 
-class MainPage extends BasePage {
-    constructor(page: Page) {
-        super(page)
-    }
+export class MainPage extends BasePage {
+  protected path = "/";
+
+  readonly registerLink: Locator;
+
+  constructor(page: Page) {
+    super(page);
+    this.registerLink = this.registerLink = page.getByRole("link", {
+      name: "Register",
+    });
+  }
+
+  async goToRegisterPage() {
+    await this.registerLink.click();
+  }
 }
