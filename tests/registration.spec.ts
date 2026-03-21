e3e512eddd96db8e3cd5f8aeee0cb50dcb7e7ee9
Ivan Osipov <bamby4a364@gmail.com>
2026-03-21 21:34:24 +0300

    поправил файл тестов

--- tests/registration.spec.ts
+++ tests/registration.spec.ts
@@ -8,9 +8,7 @@ test.describe("Регистрация пользователя", () => {
     app = new AppFacade(page)
   });
 
-  test("TC-REGRESSION-04: Успешная регистрация пользователя", async ({
-    page,
-  }) => {
+  test("TC-REGRESSION-04: Успешная регистрация пользователя", async () => {
     const user = new UserBuilder().build();
 
     await app.openRegisterPage();
@@ -18,42 +16,42 @@ test.describe("Регистрация пользователя", () => {
     await successPage.checkSuccess(user.email);
   });
 
-  test("TC-REGRESSION-05: Невалидный email", async ({ page }) => {
+  test("TC-REGRESSION-05: Невалидный email", async () => {
     const user = new UserBuilder().withEmail("@s").build();
 
     await app.openRegisterPage();
     await app.registerUserWithError(user, "Email", /wrong email/i);
   });
 
-  test("TC-REGRESSION-06: Email не заполнен", async ({ page }) => {
+  test("TC-REGRESSION-06: Email не заполнен", async () => {
     const user = new UserBuilder().withEmail("").build(); //пустой email
 
     await app.openRegisterPage();
     await app.registerUserWithError(user, "Email", /email is required./i);
   });
 
-  test("TC-REGRESSION-07: Имя не заполнено", async ({ page }) => {
+  test("TC-REGRESSION-07: Имя не заполнено", async () => {
     const user = new UserBuilder().withFirstName("").build(); //пустое имя
 
     await app.openRegisterPage();
     await app.registerUserWithError(user, "FirstName", /first name is required./i);
   });
 
-  test("TC-REGRESSION-08: Фамилия не заполнена", async ({ page }) => {
+  test("TC-REGRESSION-08: Фамилия не заполнена", async () => {
     const user = new UserBuilder().withLastName("").build(); //пустая фамилия
 
     await app.openRegisterPage();
     await app.registerUserWithError(user, "LastName", /last name is required./i);
   });
 
-  test("TC-REGRESSION-09: Пароль не заполнен", async ({ page }) => {
+  test("TC-REGRESSION-09: Пароль не заполнен", async () => {
     const user = new UserBuilder().withPassword("").build(); //пустой пароль
 
     await app.openRegisterPage();
     await app.registerUserWithError(user, "Password", /password is required./i);
   });
 
-  test("TC-REGRESSION-10: Пароль не совпадает", async ({ page }) => {
+  test("TC-REGRESSION-10: Пароль не совпадает", async () => {
     const user = new UserBuilder().build();
     const notMatchedConfirmPassword = user.password + "1";
 
@@ -61,7 +59,7 @@ test.describe("Регистрация пользователя", () => {
     await app.registerUserWithError(user, "ConfirmPassword", /the password and confirmation password do not match./i, notMatchedConfirmPassword);
   });
 
-  test("TC-REGRESSION-11: Пароль не проходит по длине", async ({ page }) => {
+  test("TC-REGRESSION-11: Пароль не проходит по длине", async () => {
     const user = new UserBuilder().withPassword("12345").build(); //6 символов минимум
 
     await app.openRegisterPage();
