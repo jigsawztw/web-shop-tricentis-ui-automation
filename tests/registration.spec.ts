09c654662a148a9e29e0ae0140801ea1e6f11b79
Ivan Osipov <bamby4a364@gmail.com>
2026-03-21 14:37:16 +0300

    накинул тестов на валидацию

--- tests/registration.spec.ts
+++ tests/registration.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { test } from "@playwright/test";
 import { userFactory } from "../factories/userFactory";
 import type { User } from "../types/user";
 import { RegisterPage, MainPage } from "../src/pages/index";
@@ -23,12 +23,13 @@ test.describe("Регистрация пользователя", () => {
 
     await registerPage.openAndCheckForm();
     await registerPage.fillRegisterFields(user);
-    const successPage = await registerPage.register();
-    await successPage.checkSuccess(user.email);
+    const registerSuccessPage = await registerPage.register();
+
+    await registerSuccessPage.checkSuccess(user.email);
   });
 
   test("TC-REGRESSION-05: Невалидный email", async ({ page }) => {
-    user.email = "@s";
+    user.email = "@s"; //невалидный
 
     await mainPage.open();
     await mainPage.goToRegisterPage();
@@ -37,11 +38,11 @@ test.describe("Регистрация пользователя", () => {
     await registerPage.fillRegisterFields(user);
     await registerPage.register();
 
-    await registerPage.expectEmailError(/wrong email/i);
+    await registerPage.expectFieldValidationError("Email", /wrong email/i);
   });
 
-  test("TC-REGRESSION-06: Пустой Email", async ({ page }) => {
-    user.email = "";
+  test("TC-REGRESSION-06: Email не заполнен", async ({ page }) => {
+    user.email = ""; //пустой
 
     await mainPage.open();
     await mainPage.goToRegisterPage();
@@ -49,6 +50,70 @@ test.describe("Регистрация пользователя", () => {
     await registerPage.openAndCheckForm();
     await registerPage.fillRegisterFields(user);
     await registerPage.register();
-    await registerPage.expectEmailError(/email is required./i);
+
+    await registerPage.expectFieldValidationError("Email", /email is required./i);
+  });
+
+  test("TC-REGRESSION-07: Имя не заполнено", async ({ page }) => {
+    user.firstName = ""; //пустое имя
+
+    await mainPage.open();
+    await mainPage.goToRegisterPage();
+
+    await registerPage.openAndCheckForm();
+    await registerPage.fillRegisterFields(user);
+    await registerPage.register();
+
+    await registerPage.expectFieldValidationError("FirstName", /first name is required./i);
+  });
+
+  test("TC-REGRESSION-08: Фамилия не заполнена", async ({ page }) => {
+    user.lastName = ""; //пустая фамилия
+
+    await mainPage.open();
+    await mainPage.goToRegisterPage();
+
+    await registerPage.openAndCheckForm();
+    await registerPage.fillRegisterFields(user);
+    await registerPage.register();
+
+    await registerPage.expectFieldValidationError("LastName", /last name is required./i);
+  });
+
+  test("TC-REGRESSION-09: Пароль не заполнен", async ({ page }) => {
+    user.password = ""; //пустой пароль
+
+    await mainPage.open();
+    await mainPage.goToRegisterPage();
+
+    await registerPage.openAndCheckForm();
+    await registerPage.fillRegisterFields(user);
+    await registerPage.register();
+
+    await registerPage.expectFieldValidationError("Password", /password is required./i);
+  });
+
+  test("TC-REGRESSION-10: Пароль не совпадает", async ({ page }) => {
+    await mainPage.open();
+    await mainPage.goToRegisterPage();
+
+    await registerPage.openAndCheckForm();
+    await registerPage.fillRegisterFields(user, user.password + "1");
+    await registerPage.register();
+
+    await registerPage.expectFieldValidationError("ConfirmPassword", /the password and confirmation password do not match./i);
+  });
+
+  test("TC-REGRESSION-11: Пароль не проходит по длине", async ({ page }) => {
+    user.password = "12345";
+
+    await mainPage.open();
+    await mainPage.goToRegisterPage();
+
+    await registerPage.openAndCheckForm();
+    await registerPage.fillRegisterFields(user);
+    await registerPage.register();
+
+    await registerPage.expectFieldValidationError("Password", /the password should have at least 6 characters./i);
   });
 });
