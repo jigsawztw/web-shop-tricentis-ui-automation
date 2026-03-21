d4b188a977a3e51ea488b394038d4b7cb758c50e
Ivan Osipov <bamby4a364@gmail.com>
2026-03-21 20:51:42 +0300

    отрефачил тесты с учетом добавленного фасада

--- tests/registration.spec.ts
+++ tests/registration.spec.ts
@@ -1,16 +1,11 @@
 import { test } from "@playwright/test";
-import { UserBuilder } from "../factories/userBuilder";
-import type { User } from "../types/user";
-import { RegisterPage, MainPage } from "../src/pages/index";
+import { UserBuilder } from "../src/helpers/builders/userBuilder";
+import { AppFacade } from "../src/pages/index";
 
+let app: AppFacade;
 test.describe("Регистрация пользователя", () => {
-  let user: User;
-  let mainPage: MainPage;
-  let registerPage: RegisterPage;
-
   test.beforeEach(async ({ page }) => {
-    mainPage = new MainPage(page);
-    registerPage = new RegisterPage(page);
+    app = new AppFacade(page)
   });
 
   test("TC-REGRESSION-04: Успешная регистрация пользователя", async ({
@@ -18,105 +13,58 @@ test.describe("Регистрация пользователя", () => {
   }) => {
     const user = new UserBuilder().build();
 
-    await mainPage.open();
-    await mainPage.goToRegisterPage();
-
-    await registerPage.checkRegisterFormFields();
-    await registerPage.fillRegisterFields(user);
-    const registerSuccessPage = await registerPage.register();
-
-    await registerSuccessPage.checkSuccess(user.email);
+    await app.openRegisterPage();
+    const successPage = await app.registerUser(user);
+    await successPage.checkSuccess(user.email);
   });
 
   test("TC-REGRESSION-05: Невалидный email", async ({ page }) => {
-    const user = new UserBuilder().withEmail("@s").build(); //невалидный email
+    const user = new UserBuilder().withEmail("@s").build();
 
-    await mainPage.open();
-    await mainPage.goToRegisterPage();
-
-    await registerPage.checkRegisterFormFields();
-    await registerPage.fillRegisterFields(user);
-    await registerPage.register();
-
-    await registerPage.expectFieldValidationError("Email", /wrong email/i);
+    await app.openRegisterPage();
+    await app.registerUserWithError(user, "Email", /wrong email/i);
   });
 
   test("TC-REGRESSION-06: Email не заполнен", async ({ page }) => {
     const user = new UserBuilder().withEmail("").build(); //пустой email
 
-    await mainPage.open();
-    await mainPage.goToRegisterPage();
-
-    await registerPage.checkRegisterFormFields();
-    await registerPage.fillRegisterFields(user);
-    await registerPage.register();
-
-    await registerPage.expectFieldValidationError("Email", /email is required./i);
+    await app.openRegisterPage();
+    await app.registerUserWithError(user, "Email", /email is required./i);
   });
 
   test("TC-REGRESSION-07: Имя не заполнено", async ({ page }) => {
     const user = new UserBuilder().withFirstName("").build(); //пустое имя
 
-    await mainPage.open();
-    await mainPage.goToRegisterPage();
-
-    await registerPage.checkRegisterFormFields();
-    await registerPage.fillRegisterFields(user);
-    await registerPage.register();
-
-    await registerPage.expectFieldValidationError("FirstName", /first name is required./i);
+    await app.openRegisterPage();
+    await app.registerUserWithError(user, "FirstName", /first name is required./i);
   });
 
   test("TC-REGRESSION-08: Фамилия не заполнена", async ({ page }) => {
     const user = new UserBuilder().withLastName("").build(); //пустая фамилия
 
-    await mainPage.open();
-    await mainPage.goToRegisterPage();
-
-    await registerPage.checkRegisterFormFields();
-    await registerPage.fillRegisterFields(user);
-    await registerPage.register();
-
-    await registerPage.expectFieldValidationError("LastName", /last name is required./i);
+    await app.openRegisterPage();
+    await app.registerUserWithError(user, "LastName", /last name is required./i);
   });
 
   test("TC-REGRESSION-09: Пароль не заполнен", async ({ page }) => {
     const user = new UserBuilder().withPassword("").build(); //пустой пароль
 
-    await mainPage.open();
-    await mainPage.goToRegisterPage();
-
-    await registerPage.checkRegisterFormFields();
-    await registerPage.fillRegisterFields(user);
-    await registerPage.register();
-
-    await registerPage.expectFieldValidationError("Password", /password is required./i);
+    await app.openRegisterPage();
+    await app.registerUserWithError(user, "Password", /password is required./i);
   });
 
   test("TC-REGRESSION-10: Пароль не совпадает", async ({ page }) => {
     const user = new UserBuilder().build();
-    const confirmPassword = user.password + "1";
+    const notMatchedConfirmPassword = user.password + "1";
 
-    await mainPage.open();
-    await mainPage.goToRegisterPage();
-
-    await registerPage.checkRegisterFormFields();
-    await registerPage.fillRegisterFields(user, confirmPassword);
-    await registerPage.register();
-
-    await registerPage.expectFieldValidationError("ConfirmPassword", /the password and confirmation password do not match./i);
+    await app.openRegisterPage();
+    await app.registerUserWithError(user, "ConfirmPassword", /the password and confirmation password do not match./i, notMatchedConfirmPassword);
   });
 
   test("TC-REGRESSION-11: Пароль не проходит по длине", async ({ page }) => {
     const user = new UserBuilder().withPassword("12345").build(); //6 символов минимум
 
-    await mainPage.open();
-    await mainPage.goToRegisterPage();
-
-    await registerPage.checkRegisterFormFields();
-    await registerPage.fillRegisterFields(user);
-    await registerPage.register();
-
-    await registerPage.expectFieldValidationError("Password", /the password should have at least 6 characters./i);
+    await app.openRegisterPage();
+    await app.registerUserWithError(user, "Password", /the password should have at least 6 characters./i);
   });
 });
