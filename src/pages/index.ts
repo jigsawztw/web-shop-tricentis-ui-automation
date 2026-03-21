bc0b4b8cdc991faf999ff38276bdcb662cf90af1
Ivan Osipov <bamby4a364@gmail.com>
2026-03-21 20:50:08 +0300

    поправил индекс

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,4 @@ export { BasePage } from "./base.page"
 export { MainPage } from "./main.page";
 export { RegisterPage } from "./register.page";
 export { RegisterSuccessPage } from "./register-success.page";
+export { AppFacade } from "../facades/appFacade";
