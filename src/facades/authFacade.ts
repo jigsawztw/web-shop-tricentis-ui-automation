fa047afbb69843a4a5d8602e7012b6c95f2d5cae
Ivan Osipov <bamby4a364@gmail.com>
2026-03-22 18:02:27 +0300

    добавл файл фасада для авторизации

---
+++ src/facades/authFacade.ts
@@ -0,0 +1,38 @@
+import { Page } from "@playwright/test";
+import { LoginPage, MainPage } from "../pages";
+import type { User } from "../../types/user";
+
+export class AuthFacade {
+  private loginPage: LoginPage;
+  private mainPage: MainPage;
+
+  constructor(private page: Page) {
+    this.loginPage = new LoginPage(page);
+    this.mainPage = new MainPage(page);
+  }
+
+  async openLoginPage() {
+    await this.mainPage.open();
+    await this.mainPage.goToLoginPage();
+    await this.loginPage.checkLoginFormFields();
+  }
+
+  async loginUser(user: User) {
+    await this.loginPage.fillLoginFields(user);
+    return await this.loginPage.login();
+  }
+
+  async loginUserWithError(email: string, password: string, error: RegExp) {
+    await this.loginPage.fillLoginFields({ email, password } as User);
+    await this.loginPage.login();
+    await this.loginPage.expectLoginValidationError(error);
+  }
+
+  async logout() {
+    const logoutButton = this.page.locator("a.ico-logout");
+    if (await logoutButton.isVisible()) {
+      await logoutButton.click();
+      await logoutButton.waitFor({ state: "hidden" });
+    }
+  }
+}
