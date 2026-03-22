8a5ae0565bf1d14fa8564e4d91a2741c6e8d8ee8
Ivan Osipov <bamby4a364@gmail.com>
2026-03-22 18:01:35 +0300

    поправил индекс файл

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,3 +3,5 @@ export { MainPage } from "./main.page";
 export { RegisterPage } from "./register.page";
 export { RegisterSuccessPage } from "./register-success.page";
 export { AppFacade } from "../facades/appFacade";
+export { LoginPage } from "../pages/login.page";
+export { LoginSuccessPage } from "../pages/login-sucess.page" ;
