d1b33f52c56c8adccb5a347f1d9ecb4de505473c
Ivan Osipov <bamby4a364@gmail.com>
2026-03-22 18:07:17 +0300

    поправил facade для регистрации

---
+++ src/facades/registerFacade.ts
@@ -0,0 +1,37 @@
+import { Page } from "@playwright/test";
+import { MainPage, RegisterPage } from "../pages";
+import type { User } from "../../types/user";
+
+export class RegisterFacade {
+  private mainPage: MainPage;
+  private registerPage: RegisterPage;
+
+  constructor(private page: Page) {
+    this.mainPage = new MainPage(page);
+    this.registerPage = new RegisterPage(page);
+  }
+
+  async openRegisterPage() {
+    await this.mainPage.open();
+    await this.mainPage.goToRegisterPage();
+    await this.registerPage.checkRegisterFormFields();
+  }
+
+  async registerUser(user: User, confirmPassword?: string) {
+    await this.registerPage.fillRegisterFields(
+      user,
+      confirmPassword ?? user.password,
+    );
+    return await this.registerPage.register();
+  }
+
+  async registerUserWithError(
+    user: User,
+    field: string,
+    error: RegExp,
+    confirmPassword?: string,
+  ) {
+    await this.registerUser(user, confirmPassword);
+    await this.registerPage.expectFieldValidationError(field, error);
+  }
+}
