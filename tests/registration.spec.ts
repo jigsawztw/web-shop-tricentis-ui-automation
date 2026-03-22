9037726e68405c543047a26fb2cac3f4a45c24fb
Ivan Osipov <bamby4a364@gmail.com>
2026-03-22 18:09:20 +0300

    поправил тесты регистрации

--- tests/registration.spec.ts
+++ tests/registration.spec.ts
@@ -5,64 +5,89 @@ import { AppFacade } from "../src/pages/index";
 let app: AppFacade;
 test.describe("Регистрация пользователя", () => {
   test.beforeEach(async ({ page }) => {
-    app = new AppFacade(page)
+    app = new AppFacade(page);
   });
 
-  test("TC-REGRESSION-04: Успешная регистрация пользователя", async () => {
+  test("TC-REGRESSION-REGISTER-04: Успешная регистрация пользователя", async () => {
     const user = new UserBuilder().build();
 
-    await app.openRegisterPage();
-    const successPage = await app.registerUser(user);
+    await app.register.openRegisterPage();
+    const successPage = await app.register.registerUser(user);
     await successPage.checkSuccess(user.email);
   });
 
-  test("TC-REGRESSION-05: Невалидный email", async () => {
+  test("TC-REGRESSION-REGISTER-05: Невалидный email", async () => {
     const user = new UserBuilder().withEmail("@s").build();
 
-    await app.openRegisterPage();
-    await app.registerUserWithError(user, "Email", /wrong email/i);
+    await app.register.openRegisterPage();
+    await app.register.registerUserWithError(user, "Email", /wrong email/i);
   });
 
-  test("TC-REGRESSION-06: Email не заполнен", async () => {
+  test("TC-REGRESSION-REGISTER-06: Email не заполнен", async () => {
     const user = new UserBuilder().withEmail("").build(); //пустой email
 
-    await app.openRegisterPage();
-    await app.registerUserWithError(user, "Email", /email is required./i);
+    await app.register.openRegisterPage();
+    await app.register.registerUserWithError(
+      user,
+      "Email",
+      /email is required./i,
+    );
   });
 
-  test("TC-REGRESSION-07: Имя не заполнено", async () => {
+  test("TC-REGRESSION-REGISTER-07: Имя не заполнено", async () => {
     const user = new UserBuilder().withFirstName("").build(); //пустое имя
 
-    await app.openRegisterPage();
-    await app.registerUserWithError(user, "FirstName", /first name is required./i);
+    await app.register.openRegisterPage();
+    await app.register.registerUserWithError(
+      user,
+      "FirstName",
+      /first name is required./i,
+    );
   });
 
-  test("TC-REGRESSION-08: Фамилия не заполнена", async () => {
+  test("TC-REGRESSION-REGISTER-08: Фамилия не заполнена", async () => {
     const user = new UserBuilder().withLastName("").build(); //пустая фамилия
 
-    await app.openRegisterPage();
-    await app.registerUserWithError(user, "LastName", /last name is required./i);
+    await app.register.openRegisterPage();
+    await app.register.registerUserWithError(
+      user,
+      "LastName",
+      /last name is required./i,
+    );
   });
 
-  test("TC-REGRESSION-09: Пароль не заполнен", async () => {
+  test("TC-REGRESSION-REGISTER-09: Пароль не заполнен", async () => {
     const user = new UserBuilder().withPassword("").build(); //пустой пароль
 
-    await app.openRegisterPage();
-    await app.registerUserWithError(user, "Password", /password is required./i);
+    await app.register.openRegisterPage();
+    await app.register.registerUserWithError(
+      user,
+      "Password",
+      /password is required./i,
+    );
   });
 
-  test("TC-REGRESSION-10: Пароль не совпадает", async () => {
+  test("TC-REGRESSION-REGISTER-10: Пароль не совпадает", async () => {
     const user = new UserBuilder().build();
     const notMatchedConfirmPassword = user.password + "1";
 
-    await app.openRegisterPage();
-    await app.registerUserWithError(user, "ConfirmPassword", /the password and confirmation password do not match./i, notMatchedConfirmPassword);
+    await app.register.openRegisterPage();
+    await app.register.registerUserWithError(
+      user,
+      "ConfirmPassword",
+      /the password and confirmation password do not match./i,
+      notMatchedConfirmPassword,
+    );
   });
 
-  test("TC-REGRESSION-11: Пароль не проходит по длине", async () => {
+  test("TC-REGRESSION-REGISTER-11: Пароль не проходит по длине", async () => {
     const user = new UserBuilder().withPassword("12345").build(); //6 символов минимум
 
-    await app.openRegisterPage();
-    await app.registerUserWithError(user, "Password", /the password should have at least 6 characters./i);
+    await app.register.openRegisterPage();
+    await app.register.registerUserWithError(
+      user,
+      "Password",
+      /the password should have at least 6 characters./i,
+    );
   });
 });
