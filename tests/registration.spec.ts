c7c2e5023fa61276e67ca4248d6cd5aa32697438
Ivan Osipov <bamby4a364@gmail.com>
2026-03-23 10:44:18 +0300

    test pre-commit lint

--- tests/registration.spec.ts
+++ tests/registration.spec.ts
@@ -33,9 +33,10 @@ test.describe('Регистрация пользователя', () => {
 
 
 
+
+
+
     
-
-
 
 
     await allure.step('Проверить успешную регистрацию', async () => {await successPage.checkSuccess(user.email);})
