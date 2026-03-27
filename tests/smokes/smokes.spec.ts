532fa330a3b67d25474c1a2ebc272fd4a764c86f
Ivan Osipov <bamby4a364@gmail.com>
2026-03-27 21:46:57 +0300

    в смоках добавил фикстуру

--- tests/smokes/smokes.spec.ts
+++ tests/smokes/smokes.spec.ts
@@ -1,38 +1,32 @@
-import { test } from '@playwright/test';
+import { test } from '../../src/helpers/fixtures/user.fixture';
 import { AppFacade } from '../../src/pages/index';
 
-let app: AppFacade;
-
 test.describe('Смоки открытия страниц', () => {
-  test.beforeEach(async ({ page }) => {
-    app = new AppFacade(page);
-  });
-
-  test('TC-SMOKE-01: Открытие главной страницы', async () => {
+  test('TC-SMOKE-01: Открытие главной страницы', async ({ app }) => {
     await test.step('Перейти на главную', async () => {
       await app.main.openPageAndCheckFields();
     });
   });
 
-  test('TC-SMOKE-11: Открытие страницы регистрации', async () => {
+  test('TC-SMOKE-11: Открытие страницы регистрации', async ({ app }) => {
     await test.step('Перейти на страницу регистрации', async () => {
       await app.register.openPageAndCheckFields();
     });
   });
 
-  test('TC-SMOKE-12: Открытие страницы авторизации', async () => {
+  test('TC-SMOKE-12: Открытие страницы авторизации', async ({ app }) => {
     await test.step('Перейти на страницу авторизации', async () => {
       await app.auth.openPageAndCheckFields();
     });
   });
 
-  test('TC-SMOKE-13: Открытие корзины покупок', async () => {
+  test('TC-SMOKE-13: Открытие корзины покупок', async ({ app }) => {
     await test.step('Перейти на страницу корзины', async () => {
       await app.cart.openPageAndCheckFields();
     });
   });
 
-  test('TC-SMOKE-14: Открытие списка желаемого', async () => {
+  test('TC-SMOKE-14: Открытие списка желаемого', async ({ app }) => {
     await test.step('Перейти на страницу списка желаемого', async () => {
       await app.wishlist.openPageAndCheckFields();
     });
